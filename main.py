f2ff400bf5fb9d59fea2d6211264c284e6cb50f7
Misterkadrix <68430333+Misterkadrix@users.noreply.github.com>
2021-05-20 14:19:35 +0200

    Correctly guessed work

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ import random
 class Hangman:
     possible_words = ['becode', 'learning', 'mathematics', 'sessions']
     word_to_find = []
+    correctly_guessed_letters = []
+    wrongly_guessed_letters = []
 
     #Here i make a random choice of my list possible_words..
     #Then i take each character of the random word and i append it on the word_to_find.
     n = random.choice(possible_words)
     for x in n:
         word_to_find.append(x)
-    #End of my Loop 
-
+        correctly_guessed_letters.append("_")
+    #End of my Loop
+    print(word_to_find)
+    print(correctly_guessed_letters)
     lives = 5
     #contains a list of strings
     #To be CORRECTED
-    correctly_guessed_letters = ['_'*4]
-    wrongly_guessed_letters = []
     turn_count = 0
     error_count = 0
     def play():
@@ -37,11 +39,26 @@ class Hangman:
             else:
                 print('it is a letter')
                 bool = 1
-        print(letter)
+        #print(letter)
+        findletter = 0
+        for x in Hangman.word_to_find:
+            if x == letter:
+                findletter = 1
+                break
 
-        #for x in self.word_to_find():
 
-    print(word_to_find)
+        count = 0
+        if findletter==1:
+            for x in Hangman.word_to_find:
+                print(x)
+                if x == letter:
+                    print(str(count) + " Ici l'id")
+                    Hangman.correctly_guessed_letters[count] = x
+
+                count += 1
+        print(Hangman.correctly_guessed_letters)
+
+    #print(word_to_find)
 
 
-#Hangman.play()
+Hangman.play()
