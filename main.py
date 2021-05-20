3e2f5501fc364ba2e541a452b6398833f5a2e47c
Misterkadrix <68430333+Misterkadrix@users.noreply.github.com>
2021-05-20 11:51:49 +0200

    letter & chosen word

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import random
+
+
 
 
 class Hangman:
     possible_words = ['becode', 'learning', 'mathematics', 'sessions']
     word_to_find = []
+
+    #Here i make a random choice of my list possible_words..
+    #Then i take each character of the random word and i append it on the word_to_find.
+    n = random.choice(possible_words)
+    for x in n:
+        word_to_find.append(x)
+    #End of my Loop 
+
     lives = 5
     #contains a list of strings
     #To be CORRECTED
@@ -10,7 +21,7 @@ class Hangman:
     wrongly_guessed_letters = []
     turn_count = 0
     error_count = 0
-    def play(self):
+    def play():
         bool = 0
 
         letter = input('Enter a letter please')
@@ -28,8 +39,9 @@ class Hangman:
                 bool = 1
         print(letter)
 
-        for x in self.word_to_find()
+        #for x in self.word_to_find():
+
+    print(word_to_find)
 
 
-
-Hangman.play()
+#Hangman.play()
